ad9acf871f7c23dca82ebe9b9fc38eca203faf0b
Matheus Assunção <130656978+Matheus-Assuncao@users.noreply.github.com>
2024-11-12 11:19:27 -0300

    conxeão em arquivo separado
    deixei a conexao em um arquivo separado, e a rota está sendo controlada pelo arquivo 'routes'.

--- routes.js
+++ routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const route = express.Router();
 
+//importando BD
+const conn = require('./connectaBD');
+
+
 //Rota inicio
 route.get('/',(req,res) =>{
     res.sendFile(path.join(__dirname, 'public/html/index.html'));
@@ -9,6 +13,17 @@ route.get('/',(req,res) =>{
 
 route.get('/produtos',(req,res) =>{
     res.sendFile(path.join(__dirname, 'public/html/forms.html'))
+})
+
+//Rota formulário
+route.post('/pedido',(req,res)=>{
+    const { produto, qtd } = req.body;
+    let sql = 'INSERT INTO order_tb (objName,qtd) values(?,?)';
+    conn.query(sql,[produto,qtd],(err,resultado)=>{
+        if(err) throw err;
+        console.log(`Pedido ${produto} salvo no BD`)
+        res.send("Pedido realizado com sucesso!")
+    })
 })
 
 
